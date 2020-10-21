14d07858360eed25ff8324c062029098433b2077
clarkj99 <clark@stontech.net>
2020-10-20 21:07:07 -0400

    mods on home screen

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Img from 'gatsby-image'
 import BackgroundImage from 'gatsby-background-image'
-
+import Typed from 'react-typed'
 import styles from './hero.module.css'
 
 export default ({ data }) => (
@@ -14,7 +14,23 @@ export default ({ data }) => (
     <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
       <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
+      <p className={styles.heroTyped}>
+        <Typed
+          strings={[
+            "Personal Shopping and Styling for Women's Clothes",
+            "Wardrobe Editing",
+            "Wardrobe Maintenance",
+            "Wardrobe Renewal",
+            "Sourcing Unique Finds and Fashion Show Production"
+          ]}
+          typeSpeed={40}
+          showCursor={true}
+          loop={true}
+          backSpeed={30}
+
+        />
+      </p>
+      {/* <p>{data.shortBio.shortBio}</p> */}
     </div>
   </BackgroundImage>
 )
