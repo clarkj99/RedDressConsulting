880d5f7b89eca6561e07b8325bf32a4dc12519c7
clarkj99 <clark@stontech.net>
2020-10-31 17:46:43 -0400

    remove publishdate from article-preview

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -4,20 +4,22 @@ import Img from 'gatsby-image'
 
 import styles from './article-preview.module.css'
 
-export default ({ article }) => (
-  <div className={styles.preview}>
-    <Link to={`/blog/${article.slug}`}>
-      <Img alt={article.title} fluid={article.heroImage.fluid} className={styles.image} />
-    </Link>
-    <h3 className={styles.previewTitle}>
-      <Link style={{ textDecoration: "none" }} to={`/blog/${article.slug}`}>{article.title}</Link>
-    </h3>
-    {/* <small>{article.publishDate}</small> */}
-    <p
-      dangerouslySetInnerHTML={{
-        __html: article.description.childMarkdownRemark.html,
-      }}
-    />
-    <Link to={`/blog/${article.slug}`}>{"more >"}</Link>
-  </div>
-)
+export default ({ article }) => {
+  console.log("article", article);
+  return (
+    <div className={styles.preview}>
+      <Link to={`/blog/${article.slug}`}>
+        <Img alt={article.title} fluid={article.heroImage.fluid} className={styles.image} />
+      </Link>
+      <h3 className={styles.previewTitle}>
+        <Link style={{ textDecoration: "none" }} to={`/blog/${article.slug}`}>{article.title}</Link>
+      </h3>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: article.description.childMarkdownRemark.html,
+        }}
+      />
+      <Link to={`/blog/${article.slug}`}>{"more >"}</Link>
+    </div>
+  )
+}
