1bfc86a66ea8acde9aa5993bd6132e6acad94c37
clarkj99 <clark@stontech.net>
2020-10-31 22:59:51 -0400

    replace console log

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -5,6 +5,7 @@ import Img from 'gatsby-image'
 import styles from './article-preview.module.css'
 
 export default ({ article }) => {
+  console.log("article", article);
   return (
     <div className={styles.preview}>
       <Link to={`/blog/${article.slug}`}>
