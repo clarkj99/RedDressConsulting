f30303850261561601dcfc19c7cb25a92e1ca643
clarkj99 <clark@stontech.net>
2020-10-31 23:07:12 -0400

    use divs for dangerouslySetInnerHTML

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -5,7 +5,6 @@ import Img from 'gatsby-image'
 import styles from './article-preview.module.css'
 
 export default ({ article }) => {
-  console.log("article", article);
   return (
     <div className={styles.preview}>
       <Link to={`/blog/${article.slug}`}>
@@ -14,11 +13,11 @@ export default ({ article }) => {
       <h3 className={styles.previewTitle}>
         <Link style={{ textDecoration: "none" }} to={`/blog/${article.slug}`}>{article.title}</Link>
       </h3>
-      <p
+      <div
         dangerouslySetInnerHTML={{
           __html: article.description.childMarkdownRemark.html,
         }}
-      />
+      ></div>
       <Link to={`/blog/${article.slug}`}>{"more >"}</Link>
     </div>
   )
